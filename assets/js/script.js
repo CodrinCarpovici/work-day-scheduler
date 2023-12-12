7073683c3af82309635c0278b0bc0e92e054fee9
CodrinCarpovici <gabriel_9729@yahoo.co.uk>
2023-12-12 18:03:18 +0000

    set up basicc jquery structure and commented requirements

---
+++ assets/js/script.js
@@ -0,0 +1,9 @@
+$(document).ready(function () {
+  // Document Elements
+  // Document Variables
+  // Standard business hours timeblocks
+  // Color-code time blocks
+  // Allow the user to enter and event when they click a timeblock
+  // Save the event in local sotrage when save button is clicked
+  // Persist events between refreshes
+});
