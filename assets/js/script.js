11ada9645096cd9b9d3d150813497a4378a2e266
CodrinCarpovici <gabriel_9729@yahoo.co.uk>
2023-12-12 18:20:11 +0000

    added current day and hour to display at the top of the page using setInterval and dayjs

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,20 @@
 $(document).ready(function () {
   // Document Elements
+  const currentDay = $("#currentDay");
+
   // Document Variables
+
+  // Function to update current Time
+  const updateClock = () => {
+    currentDay.text(dayjs());
+  };
+
+  // Updating time every second
+  setInterval(updateClock, 1000);
+
+  // Initial update on page load
+  updateClock();
+  
   // Standard business hours timeblocks
   // Color-code time blocks
   // Allow the user to enter and event when they click a timeblock
